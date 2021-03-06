e0419d2a8cd4a9180c5084c0fb7aaeca2fc7bd4a
lizi1203 <1095036488@qq.com>
2021-03-06 09:30:18 +0800

    Initial commit

---
+++ app/src/main/java/com/example/graduatetravell/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.graduatetravell;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //注册点击事件
+        Button goToRegister_button = (Button)findViewById(R.id.goToRegister_button);
+        goToRegister_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/example/graduatetravell/RegisterActivity.java
@@ -0,0 +1,14 @@
+package com.example.graduatetravell;
+
+import android.app.Activity;
+
+import android.os.Bundle;
+
+public class RegisterActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
