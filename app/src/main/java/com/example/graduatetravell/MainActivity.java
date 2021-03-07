c7857de148cd4349af7adacbc62af5850ab245fe
lizi1203 <1095036488@qq.com>
2021-03-07 11:41:29 +0800

    add Mine headListItem

--- app/src/main/java/com/example/graduatetravell/MainActivity.java
+++ app/src/main/java/com/example/graduatetravell/MainActivity.java
@@ -6,15 +6,100 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
+
+    private ViewPager mainViewPager;
+    private RadioGroup mainTabRadioGroup;
+
+    private List<Fragment> mainFragments;
+    private FragmentPagerAdapter mainAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
-
+        initView();
     }
+
+    private void initView() {
+        //find view
+        mainViewPager = findViewById(R.id.main_viewPage);
+        mainTabRadioGroup = findViewById(R.id.mainTabs_radioGroup);
+        //init fragment
+        mainFragments = new ArrayList<>(4);
+        mainFragments.add(BlankFragment.newInstance("游记","1"));
+        mainFragments.add(BlankFragment.newInstance("热门","2"));
+        mainFragments.add(BlankFragment.newInstance("新闻","3"));
+        mainFragments.add(BlankFragment.newInstance("我的","4"));
+        //init ViewPager
+        mainAdapter = new FragmentPagerAdapter(getSupportFragmentManager(),4) {
+            @NonNull
+            @Override
+            public Fragment getItem(int position) {
+                return mainFragments == null?null:mainFragments.get(position);
+            }
+
+            @Override
+            public int getCount() {
+                return mainFragments == null? 0 : mainFragments.size();
+            }
+        };
+        mainViewPager.setAdapter(mainAdapter);
+        //注册监听者
+        mainViewPager.addOnPageChangeListener(mainPageChangeListener);
+        mainTabRadioGroup.setOnCheckedChangeListener(mainOnCheckedChangeListener);
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        mainViewPager.removeOnPageChangeListener(mainPageChangeListener);
+    }
+
+    private ViewPager.OnPageChangeListener mainPageChangeListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            RadioButton radioButton = (RadioButton)mainTabRadioGroup.getChildAt(position);
+            radioButton.setChecked(true);
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
+
+    private  RadioGroup.OnCheckedChangeListener mainOnCheckedChangeListener = new RadioGroup.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(RadioGroup group, int checkedId) {
+            for(int i = 0; i < group.getChildCount();i++){
+                if(group.getChildAt(i).getId() == checkedId){
+                    mainViewPager.setCurrentItem(i);
+                    return;
+                }
+            }
+        }
+    };
 
 }
